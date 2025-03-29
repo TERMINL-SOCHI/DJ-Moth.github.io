ad2fd84aa43edce0dac2b9e7d4be0c476263b555
Joshua Miller <pipinisacat@gmail.com>
2025-03-29 14:55:34 +1300

    changed the comment

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Simple navigation log
+// All this does is just log usage of the navigation system
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Welcome to my website!");
 });
